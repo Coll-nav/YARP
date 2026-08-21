9f02f8fc672521ebf7bc1873d33e17874fa416f3
Coll-nav <navoev.u@gmail.com>
2026-08-21 18:45:43 +0300

    Add Redis

--- GateKeeper.ApiGateway/Program.cs
+++ GateKeeper.ApiGateway/Program.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,7 +31,30 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
         };
     });
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+});
+
+builder.Services.AddRateLimiter(options => 
+    options.AddSlidingWindowLimiter("SlidingPolicy", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 5;
+        limiterOptions.Window = TimeSpan.FromSeconds(10);
+        limiterOptions.SegmentsPerWindow = 2;
+        limiterOptions.QueueLimit = 0;
+        limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    })
+);
+
+builder.Services.AddStackExchangeRedisOutputCache(options => 
+    options.Configuration = builder.Configuration.GetConnectionString("Redis")
+    ?? throw new InvalidOperationException("Redis connection string is required"));
+builder.Services.AddOutputCache(options => 
+    options.AddPolicy("RedisPolicy", policy =>
+    {
+        policy.Expire(TimeSpan.FromMinutes(1));
+    }));
 
 var app = builder.Build();
 
@@ -41,8 +66,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseRateLimiter();
+
+app.UseOutputCache();
 
 app.MapReverseProxy();
 
--- GateKeeper.TokenGenerator/Program.cs
+++ GateKeeper.TokenGenerator/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Security.Principal;
 
 var issuer = "GateKeeper.Auth";
 var audience = "GateKeeper.Api";
 var key = "f7f37e14-1e00-49ea-8d79-6725ca75ca09";
 
-//создаем скретный ключ 
+List<Claim> claims = new List<Claim>();
+claims.Add(new Claim(ClaimTypes.Role, "Admin" ));
+
+
+//преобразуем скретный ключ
 var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
 //подписываем его HMAC-256
 var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
@@ -15,7 +21,8 @@ var token = new JwtSecurityToken(
     issuer : issuer, 
     audience : audience,
     expires: DateTime.UtcNow.AddMinutes(120),
-    signingCredentials: signingCredentials);
+    signingCredentials: signingCredentials,
+    claims: claims);
 //добавляем обработчик 
 var  handler = new JwtSecurityTokenHandler();
 //сереализуем в строку для чтения
